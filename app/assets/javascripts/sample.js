1b9c26c420414d3da6ea3443a78daade80db760a
Siva Prasad <sivaprasad@NT-SYS-231example.com>
2013-11-14 12:54:18 +0530

    added the only number condition for phone

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
-$('#body_content_title').hide();
-$('#program-section').css("display","none");
-$('#myTab1').css("display","none");
-$('#why-smartone-block').css("display","none");
-$('#myTab').css("display","none");
-$('#whyPartnerTab').css("display","none");
+  $('#body_content_title').hide();
+  $('#program-section').css("display","none");
+  $('#myTab1').css("display","none");
+  $('#why-smartone-block').css("display","none");
+  $('#myTab').css("display","none");
+  $('#keypartnersTab').css("display","none");
+  $('#whyPartnerTab').css("display","none");
 });
 
-function NumericCharKeyPress(e){
-  var charCode = e.keyCode? e.keyCode : e.charCode;
-  if (charCode != 190 && charCode > 31 && 
-       (charCode < 48 || charCode > 57) && 
-       (charCode < 96 || charCode > 105) && 
-       (charCode < 37 || charCode > 40) && 
-        charCode != 110 && charCode != 8 && charCode != 46 )
-    e.preventDefault();    
-  else
-    return true;
-  return false;
+function NumericCharKeyPress(evt){
+  var charCode = (evt.which) ? evt.which : event.keyCode;  
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
 }
-
