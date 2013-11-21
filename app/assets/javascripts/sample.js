3d696198306a6af1accba02b6a3c78694cc5138d
Siva Prasad <sivaprasad@NT-SYS-231example.com>
2013-11-21 13:07:50 +0530

    minor css changes once after going to contact page

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
   $('#myTab').css("display","none");
   $('#keypartnersTab').css("display","none");
   $('#whyPartnerTab').css("display","none");
+  
+  
+  var sub_menu = window.location.href.split("/#")[1];
+  if(sub_menu == "main-about"){
+    $('#main-navbar ul li').removeClass('active');
+    $('#abt').addClass('active');
+   $(window).scrollTop($("#main-about").offset().top-100);
+ }
+ if(sub_menu == "programs"){
+   $('#main-navbar ul li').removeClass('active');
+   $('#prgs').addClass('active');
+   $(window).scrollTop($("#programs").offset().top-50);
+ }
 });
 
 function NumericCharKeyPress(evt){
@@ -14,3 +27,4 @@ function NumericCharKeyPress(evt){
     return false;
   return true;
 }
+
