f4717c718bcfd5c86e5a2d9bf9834f8ad6f03727
Siva Prasad <sivaprasad@NT-SYS-231example.com>
2014-03-19 14:22:43 +0530

    commented the signup routes for user

--- vendor/extensions/sign_ups/config/routes.rb
+++ vendor/extensions/sign_ups/config/routes.rb
@@ -1,12 +1,12 @@
 Refinery::Core::Engine.routes.draw do
   # Frontend routes
-  namespace :sign_ups do
-    resources :sign_ups, :path => '', :only => [:new, :create] do
-      collection do
-        get :thank_you
-      end
-    end
-  end
+#  namespace :sign_ups do
+#    resources :sign_ups, :path => '', :only => [:new, :create] do
+#      collection do
+#        get :thank_you
+#      end
+#    end
+#  end
 
   # Admin routes
   namespace :sign_ups, :path => '' do
